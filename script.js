1a5e48299916536bbe3e4e9373904ca00e9139e2
bujak.rafal@yahoo.com <bujak.rafal@yahoo.com>
2018-06-19 00:27:17 +0200

    s

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://obscure-ocean-73838.herokuapp.com/v1/';
+  const apiRoot = 'https://obscure-ocean-73838.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://obscure-ocean-73838.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
